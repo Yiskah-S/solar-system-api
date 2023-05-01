2bd2bc33a67189116d1d17d7b0fbd16f19d0ce40
Jessica <Yiskah.Scientia@gmail.com>
2023-05-01 09:46:17 -0600

    Add planet class

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,5 @@
 from flask import Blueprint, jsonify, abort, make_response
 
-class Planet:
-    def __init__(self, id, name, description, earth_masses):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.earth_masses = earth_masses
-
-    def to_dict(self):
-        return dict(
-            id = self.id, 
-            name = self.name, 
-            description = self.description, 
-            mass = self.mass
-        )
 
 planet_list = [
     Planet(1, "Mercury", "Small, rocky planet closest to the Sun", 0.330),
@@ -54,3 +40,87 @@ def validate_planet(id):
             return planet
 
     abort(make_response({"message": f"Planet {id} is not found!"}, 404))
+
+# # Wave 03: Connecting the Database, Read and Create Endpoints
+
+# ## Database Setup
+
+# Complete the following setup steps of the Solar System API repo:
+# 1. Activate the virtual environment
+# 1. Create the database `solar_system_development`
+#     * *Every member of the group must create the database on their computer*
+# 1. Setup the `Planet` model with the attributes `id`, `name`, and `description`, and one additional attribute
+# 1. Create a migration to add a table for the `Planet` model and then apply it. 
+#     * *Confirm that the `planet` table has been created as expected in postgres*.
+
+# ## RESTful Endpoints: Create and Read
+
+# Create or refactor the following endpoints, with similar functionality presented in the Hello Books API:
+
+# As a client, I want to send a request...
+
+# 1. ...with new valid `planet` data and get a success response, so that I know the API saved the planet data
+# 1. ...to get all existing `planets`, so that I can see a list of planets, with their `id`, `name`, `description`, and other data of the `planet`.
+
+# # Wave 04: Read, Update, Delete
+
+# ## RESTful Endpoints: Read, Update, and Delete
+
+# Create the following endpoints, with similar functionality presented in the Hello Books API:
+
+# As a client, I want to send a request...
+
+# 1. ...to get one existing `planet`, so that I can see the `id`, `name`, `description`, and other data of the `planet`.
+# 1. ... with valid planet data to update one existing `planet` and get a success response, so that I know the API updated the `planet` data.
+# 1. ... to delete one existing `planet` and get a success response, so that I know the API deleted the `planet` data..
+#     * Each of the above endpoints should respond with a `404` for non-existing planets and a `400` for invalid `planet_id`.
+
+# # Wave 05: Review and Refactor
+
+# Review the requirements for Wave 01 - 04
+# * Test the endpoints using postman
+# * Complete or fix any incomplete or broken endpoints
+# * Look for opportunities to refactor
+
+# As time allows, add custom routes. 
+# * Consider using query params
+
+# # Wave 06: Writing Tests
+
+# ## Setup
+
+# Complete the following requirements, with similar functionality to the Hello Books API:
+
+# 1. Create a `.env` file.
+# 1. Populate it with two environment variables: `SQLALCHEMY_DATABASE_URI` and `SQLALCHEMY_TEST_DATABASE_URI`. Set their values to the appropriate connection strings.
+# 1. Create a test database with the correct, matching name.
+# 1. Refactor the `create_app` method to:
+#    * Check for a configuration flag
+#    * Read the correct database location from the appropriate environment variables
+# 1. Manually test that our development environment still works.
+# 1. Create a `tests` folder with the files:
+#     -  `tests/__init__.py`
+#     -  `tests/conftest.py`
+#     -  `tests/test_routes.py`.
+# 1. Populate `tests/conftest.py` with the recommended configuration.
+# 1. Create a test to check `GET` `/planets` returns `200` and an empty array.
+# 1. Confirm this test runs and passes.
+
+# ## Writing Tests
+
+# Create test fixtures and unit tests for the following test cases:
+
+# 1. `GET` `/planets/1` returns a response body that matches our fixture
+# 1. `GET` `/planets/1` with no data in test database (no fixture) returns a `404`
+# 1. `GET` `/planets` with valid test data (fixtures) returns a `200` with an array including appropriate test data
+# 1. `POST` `/planets` with a JSON request body returns a `201`
+
+# ## Code Coverage
+
+# Check your code coverage using `pytest-cov`. Review the [code coverage exercise](https://github.com/adaGold/code-coverage-exercise) on how to use `pytest-cov` to generate a code coverage report. We will need to change the directory where the application code is located from `student` to `app`.
+
+# `pytest --cov=app --cov-report html --cov-report term`
+
+# For this project, we will not expect to have high test coverage because we have not tested all of our CRUD routes. Still, it is helpful to practice checking coverage and reading reports of the code which detail the code that is tested, and the code that is not tested.
+
+
